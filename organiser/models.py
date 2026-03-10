e1da9b8332e488e2f28954f04d039bdc76a40a95
Aria608 <3153302Z@student.gla.ac.uk>
2026-03-10 08:59:27 +0800

    Update models.py

--- organiser/models.py
+++ organiser/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from accounts.models import User
+from activities.models import Activity
 
-# Create your models here.
+
+class Application(models.Model):
+
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    )
+
+    volunteer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="applications"
+    )
+
+    activity = models.ForeignKey(
+        Activity,
+        on_delete=models.CASCADE,
+        related_name="applications"
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    attended = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.volunteer} - {self.activity}"
