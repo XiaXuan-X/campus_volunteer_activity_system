55e9a1e624068a7ffa2e2a571f71c83c34064000
Aria608 <3153302Z@student.gla.ac.uk>
2026-03-10 08:58:44 +0800

    Update models.py

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Activity(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    location = models.CharField(max_length=200)
+
+    start_date = models.DateField()
+
+    start_time = models.TimeField()
+
+    end_date = models.DateField()
+
+    end_time = models.TimeField()
+
+    max_volunteers = models.IntegerField()
+
+    organiser = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="created_activities"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
