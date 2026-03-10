71bdba21455070957341a96c9398236251c80660
Aria608 <3153302Z@student.gla.ac.uk>
2026-03-10 08:57:52 +0800

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('volunteer', 'Volunteer'),
+        ('organiser', 'Organiser'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='volunteer'
+    )
+
+    phone = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    student_id = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
